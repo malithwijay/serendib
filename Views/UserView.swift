31dabe60d7625c4f9fbd571ad74b32c4927b4c20
malithwijay <malith.nipun@gmail.com>
2024-03-29 17:01:14 +0530

    changes to product card. user profile created.

---
+++ Views/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  Serandib
+//
+//  Created by Malith Wijayathunga on 2024-03-29.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserView()
+}
